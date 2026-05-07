1a3f6e5a6ae93e602f8c041f4b7d74ba926d6b1b
florousseau <commercial.rousseauflorent@gmail.com>
2026-05-08 01:11:51 +0200

    fix #3

--- red/ssh-bf.py
+++ red/ssh-bf.py
@@ -22,8 +22,8 @@ def try_ssh(ip, port, username, password):
 def main():
     ip = "10.12.0.10"
     port = 22
-    users = "/home/mininet/LINFO2347/repo/bf/users.txt"
-    passwords = "/home/mininet/LINFO2347/repo/bf/passwords.txt"
+    users = "/home/mininet/LINFO2347/repo/red/bf/users.txt"
+    passwords = "/home/mininet/LINFO2347/repo/red/bf/passwords.txt"
 
     with open(users, "r") as f:
         usernames = [line.strip() for line in f]
